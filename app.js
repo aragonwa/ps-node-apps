c50d613140f287ddfa3f1e375036d4e87a394d93
Alex Aragon <alexaragon@MacBook-Pro-2.local>
2020-01-07 20:44:19 -0800

    initia

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const chalk = require('chalk');
+const morgan = require('morgan');
+const debug = require('debug')('app');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'My Library', list: ['a', 'b'] });
+});
+
+app.listen(port, () => {
+  debug(chalk.green(`Listening on port ${port}`));
+});
