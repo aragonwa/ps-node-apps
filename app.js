f4b33e67484127d53ec5880083df3d36ce4c4741
Alex Aragon <alexaragon@MacBook-Pro-2.local>
2020-01-11 07:01:31 -0800

    latest

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ const chalk = require('chalk');
 const morgan = require('morgan');
 const debug = require('debug')('app');
 const path = require('path');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({secret: "library"}));
+
+require('./src/config/passport.js')(app);
+
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
@@ -18,10 +29,11 @@ app.set('view engine', 'ejs');
 const nav = [{ link: '/books', title: 'Books' }, { link: '/authors', title: 'Author' }];
 const bookRouter = require('./src/routes/bookRoutes')(nav);
 const adminRouter = require('./src/routes/adminRoutes')(nav);
-
+const authRouter = require('./src/routes/authRoutes')(nav);
 
 app.use('/books', bookRouter);
 app.use('/admin', adminRouter);
+app.use('/auth', authRouter);
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'My Library', nav: [{ link: '/books', title: 'Books' }, { link: '/authors', title: 'Author' }] });
@@ -30,4 +42,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   debug(chalk.green(`Listening on port ${port}`));
 });
-;
